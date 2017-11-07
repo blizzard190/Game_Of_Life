03fdabdafce8fc3f5f63c90f14556f7811c65e1f
blizzard190 <22256@ma-web.nl>
2017-11-07 19:02:56 +0100

    Upload
    Upload final version

---
+++ GameOfLife/Assets/Script/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameOfLife game;
+
+    public float cameraSpeed = 10f;
+    public float zoomSpeed = 7f;
+
+    // just check keyboard input and change camera position
+    // camera position is clamped to game size
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 newPos = transform.position;
+        newPos.x += Time.deltaTime * x * cameraSpeed;
+        newPos.y += Time.deltaTime * y * cameraSpeed;
+
+        newPos.x = Mathf.Clamp(newPos.x, 0f, (float)(game.sizeX - 1));
+        newPos.y = Mathf.Clamp(newPos.y, 0f, (float)(game.sizeY - 1));
+
+        transform.position = newPos;
+
+        if (Input.GetKey(KeyCode.Equals))
+            Zoom(zoomSpeed);
+        else if (Input.GetKey(KeyCode.Minus))
+            Zoom(-zoomSpeed);
+    }
+
+    // primitive zoom method which changes cameras z position
+    private void Zoom(float z)
+    {
+        Vector3 newPos = transform.position;
+        newPos.z += Time.deltaTime * z;
+        newPos.z = Mathf.Clamp(newPos.z, -50f, -10f);
+        transform.position = newPos;
+    }
+}
---
+++ GameOfLife/Assets/Script/D_Array.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_Array : MonoBehaviour {
+    public GameObject boep;
+    public int ColumnLenght;
+    public int Row;
+    public GameObject[,] Array;
+    [SerializeField]
+    private Material mat;
+
+    private MeshRenderer colour;
+
+    private float duration = 1.0F;
+
+    void Start ()
+    {
+        Array = new GameObject[ColumnLenght, Row];
+        for(int i = 0; i < ColumnLenght; i++)
+        {
+            for(int j = 0; j < Row; j++)
+            {
+                Array[i, j] = (GameObject)Instantiate(boep, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+	}
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            StartCoroutine("Fade");
+        }
+    }
+
+    private IEnumerator Fade()
+    {
+        for (float i = 0; i < 1; i += 0.01f)
+        {
+            mat.color = new Color(1, i, i);
+            yield return null;
+        }
+        //float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        //colour.material.color = Color.Lerp(Color.green, Color.blue, lerp);
+        //yield return null;  
+    }
+}
